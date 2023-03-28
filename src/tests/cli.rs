ae39c46e02521f58c35e46cebbfcd2fe7e62d6c3
Fionn Langhans <fionn.langhans@gmail.com>
2023-03-28 07:19:07 +0200

    Added more tests

--- src/tests/cli.rs
+++ src/tests/cli.rs
@@ -2,6 +2,27 @@ use assert_cmd::Command;
 use predicates::prelude::*;
 
 #[test]
+fn test_nothing() -> anyhow::Result<()> {
+    let mut cmd = Command::cargo_bin("pgcl")?;
+
+    cmd.assert().success().stdout(predicate::str::is_empty());
+
+    Ok(())
+}
+
+#[test]
+fn test_two_values() -> anyhow::Result<()> {
+    let mut cmd = Command::cargo_bin("pgcl")?;
+
+    cmd.write_stdin("1\n2")
+        .assert()
+        .success()
+        .stdout(predicate::str::is_match(r"^1\n2\n$").unwrap());
+
+    Ok(())
+}
+
+#[test]
 fn test_value_1() -> anyhow::Result<()> {
     let mut cmd = Command::cargo_bin("pgcl")?;
 
